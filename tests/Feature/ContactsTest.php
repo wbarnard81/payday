308d296b9eb5f56f524010e03471e004bd43bbbb
Victor Gonzalez <victor@coderstape.com>
2019-07-21 19:33:22 -0400

    deleting contacts

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -79,8 +79,6 @@ class ContactsTest extends TestCase
     /** @test */
     public function a_contact_can_be_patched()
     {
-        $this->withoutExceptionHandling();
-
         $contact = factory(Contact::class)->create();
 
         $response = $this->patch('/api/contacts/' . $contact->id, $this->data());
@@ -91,6 +89,16 @@ class ContactsTest extends TestCase
         $this->assertEquals('test@email.com', $contact->email);
         $this->assertEquals('05/14/1988', $contact->birthday->format('m/d/Y'));
         $this->assertEquals('ABC String', $contact->company);
+    }
+
+    /** @test */
+    public function a_contact_can_be_deleted()
+    {
+        $contact = factory(Contact::class)->create();
+
+        $response = $this->delete('/api/contacts/' . $contact->id);
+
+        $this->assertCount(0, Contact::all());
     }
 
     private function data()
